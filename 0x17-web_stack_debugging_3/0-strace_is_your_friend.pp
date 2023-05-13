630ee0f367f5b20e32886e2123c3655dc81bb87f
Vincent <vwan045@gmail.com>
2023-05-14 01:01:20 +0300

    WEB debbuging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Correct a typo in the settings file
 exec { '/var/html/www/wp-settings.php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/',
